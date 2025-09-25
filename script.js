20acf89027ec661097b7ceb9bcd72e6613cb3954
od-8 <178620777+od-8@users.noreply.github.com>
2025-09-25 22:43:24 +0100

    feat: Added some more functionality and added more buttons

--- script.js
+++ script.js
@@ -1,19 +1,92 @@
-let result = document.querySelector("#result");
+let display = document.querySelector("#display");
 
-const equalBtn = document.querySelector("#equal");
-equalBtn.addEventListener("click", () => {result.textContent += "="})
+// Numbers
+const one = document.querySelector("#one");
+one.addEventListener("click", () => {display.textContent += "1"});
 
-const clearBtn = document.querySelector("#clear")
-clearBtn.addEventListener("click", () => {})
+const two = document.querySelector("#two");
+two.addEventListener("click", () => {display.textContent += "2"});
 
-const addBtn = document.querySelector("#add");
-addBtn.addEventListener("click", () => {result.textContent += "+"})
+const three = document.querySelector("#three");
+three.addEventListener("click", () => {display.textContent += "3"});
 
-const subtractBtn = document.querySelector("#subtract");
-subtractBtn.addEventListener("click", () => {result.textContent += "-"})
+const four = document.querySelector("#four");
+four.addEventListener("click", () => {display.textContent += "4"});
 
-const multipyBtn = document.querySelector("#multiply");
-multipyBtn.addEventListener("click", () => {result.textContent += "*"})
+const five = document.querySelector("#five");
+five.addEventListener("click", () => {display.textContent += "5"});
 
-const divideBtn = document.querySelector("#divide");
-divideBtn.addEventListener("click", () => {result.textContent += "/"})
+const six = document.querySelector("#six");
+six.addEventListener("click", () => {display.textContent += "6"});
+
+const seven = document.querySelector("#seven");
+seven.addEventListener("click", () => {display.textContent += "7"});
+
+const eight = document.querySelector("#eight");
+eight.addEventListener("click", () => {display.textContent += "8"});
+
+const nine = document.querySelector("#nine");
+nine.addEventListener("click", () => {display.textContent += "9"});
+
+const zero = document.querySelector("#zero");
+zero.addEventListener("click", () => {display.textContent += "0"});
+
+// Operators
+const add = document.querySelector("#add");
+add.addEventListener("click", () => {display.textContent += "+"})
+
+const subtract = document.querySelector("#subtract");
+subtract.addEventListener("click", () => {display.textContent += "-"})
+
+const multipy = document.querySelector("#multiply");
+multipy.addEventListener("click", () => {display.textContent += "*"})
+
+const divide = document.querySelector("#divide");
+divide.addEventListener("click", () => {display.textContent += "/"});
+
+const clear = document.querySelector("#clear");
+clear.addEventListener("click", () => {display.textContent = ""});
+
+const equal = document.querySelector("#equal");
+equal.addEventListener("click", peformCalculation);
+
+
+function peformCalculation() {
+    let inputArr = display.textContent.split("")
+    let firstNum = "";
+    let operator = "";
+    let lastNum = "";
+    let answer = null;
+
+    for(let i = 0; i < inputArr.length; i ++) {
+        if(Number.isInteger(parseInt(inputArr[i])) && operator == "") {
+            firstNum += inputArr[i];
+        } else if (Number.isInteger(parseInt(inputArr[i])) && operator != "") {
+            lastNum += inputArr[i];
+        } else {
+            operator += inputArr[i];
+        };
+    }
+
+    answer = solveEquation(operator, parseInt(firstNum), parseInt(lastNum));
+
+    console.log(answer);
+}
+
+function solveEquation(operator, firstNum, lastNum) {
+    switch (operator) {
+        case "+":
+            return addition(firstNum, lastNum);
+        case "-":
+            return subtraction(firstNum, lastNum);
+        case "*":
+            return multiplication(firstNum, lastNum);
+        case "/":
+            return division(firstNum, lastNum);
+    }
+}
+
+function addition(firstNum, lastNum) { return firstNum + lastNum };
+function subtraction(firstNum, lastNum) { return firstNum - lastNum };
+function multiplication(firstNum, lastNum) { return firstNum * lastNum };
+function division(firstNum, lastNum) { return firstNum / lastNum };
