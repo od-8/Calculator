be35e60b105b517c7b2d4553bd4818fdf38624eb
od-8 <178620777+od-8@users.noreply.github.com>
2025-10-07 22:21:55 +0100

    feat: Validation of expression

--- script.js
+++ script.js
@@ -1,92 +1,117 @@
 let display = document.querySelector("#display");
+let total = 0;
 
 // Numbers
 const one = document.querySelector("#one");
-one.addEventListener("click", () => {display.textContent += "1"});
+one.addEventListener("click", () => { display.textContent += 1; });
 
 const two = document.querySelector("#two");
-two.addEventListener("click", () => {display.textContent += "2"});
+two.addEventListener("click", () => { display.textContent += 2; });
 
 const three = document.querySelector("#three");
-three.addEventListener("click", () => {display.textContent += "3"});
+three.addEventListener("click", () => { display.textContent += 3; });
 
 const four = document.querySelector("#four");
-four.addEventListener("click", () => {display.textContent += "4"});
+four.addEventListener("click", () => { display.textContent += 4; });
 
 const five = document.querySelector("#five");
-five.addEventListener("click", () => {display.textContent += "5"});
+five.addEventListener("click", () => { display.textContent += 5; });
 
 const six = document.querySelector("#six");
-six.addEventListener("click", () => {display.textContent += "6"});
+six.addEventListener("click", () => { display.textContent += 6; });
 
 const seven = document.querySelector("#seven");
-seven.addEventListener("click", () => {display.textContent += "7"});
+seven.addEventListener("click", () => { display.textContent += 7; });
 
 const eight = document.querySelector("#eight");
-eight.addEventListener("click", () => {display.textContent += "8"});
+eight.addEventListener("click", () => { display.textContent += 8; });
 
 const nine = document.querySelector("#nine");
-nine.addEventListener("click", () => {display.textContent += "9"});
+nine.addEventListener("click", () => { display.textContent += 9; });
 
 const zero = document.querySelector("#zero");
-zero.addEventListener("click", () => {display.textContent += "0"});
+zero.addEventListener("click", () => { display.textContent += 0; });
 
 // Operators
 const add = document.querySelector("#add");
-add.addEventListener("click", () => {display.textContent += "+"})
+add.addEventListener("click", () => { display.textContent += "+"; });
 
 const subtract = document.querySelector("#subtract");
-subtract.addEventListener("click", () => {display.textContent += "-"})
+subtract.addEventListener("click", () => { display.textContent += "-"; });
 
 const multipy = document.querySelector("#multiply");
-multipy.addEventListener("click", () => {display.textContent += "*"})
+multipy.addEventListener("click", () => { display.textContent += "*"; });
 
 const divide = document.querySelector("#divide");
-divide.addEventListener("click", () => {display.textContent += "/"});
+divide.addEventListener("click", () => { display.textContent += "/"; });
 
 const clear = document.querySelector("#clear");
-clear.addEventListener("click", () => {display.textContent = ""});
+clear.addEventListener("click", () => {
+    display.textContent = "";
+    total = 0;
+});
 
 const equal = document.querySelector("#equal");
-equal.addEventListener("click", peformCalculation);
+equal.addEventListener("click", validExpression);
 
+function validExpression() {
+    let exp = display.textContent.split('');
 
-function peformCalculation() {
-    let inputArr = display.textContent.split("")
-    let firstNum = "";
-    let operator = "";
-    let lastNum = "";
-    let answer = null;
+    if (validSymbols(exp) == false) { display.textContent = ""; }
 
-    for(let i = 0; i < inputArr.length; i ++) {
-        if(Number.isInteger(parseInt(inputArr[i])) && operator == "") {
-            firstNum += inputArr[i];
-        } else if (Number.isInteger(parseInt(inputArr[i])) && operator != "") {
-            lastNum += inputArr[i];
-        } else {
-            operator += inputArr[i];
-        };
-    }
+    determineTotal(exp)
 
-    answer = solveEquation(operator, parseInt(firstNum), parseInt(lastNum));
+    evaluateExpression(exp)
 
-    console.log(answer);
+    console.log(total)
 }
 
-function solveEquation(operator, firstNum, lastNum) {
-    switch (operator) {
+function validSymbols(exp) {
+    let symbols = 0;
+
+    for(let i = 0; i < exp.length; i++) {
+        if (["+", "-", "*", "/"].includes(exp[i]) && i != 0) symbols += 1;
+    }
+
+    if (symbols != 1 || ["+", "*", "/"].includes(exp[0])) return false;
+
+    return true;
+}
+
+function determineTotal(exp) {
+    let num = "";
+
+    for(let i = 0; i < exp.length; i++) {
+        if (i == 0 || Number.isInteger(parseInt(exp[i]))) { num += exp[i]; };
+        if (i != 0 && Number.isInteger(parseInt(exp[i])) == false) { break; };
+    }
+
+    total = parseInt(num);
+}
+
+function evaluateExpression(exp) {
+    for(let i = 0; i < exp.length; i++) {
+        if (i != 0 && ["+", "-", "*", "/"].includes(exp[i])) {
+            let secondNum = parseInt(exp.slice(i + 1).join(""));
+            calculateTotal(total, exp[i], secondNum);
+            break;
+        }
+    }
+}
+
+function calculateTotal(total, symbol, secondNum) {
+    switch(symbol) {
         case "+":
-            return addition(firstNum, lastNum);
+            total += secondNum;
+            break;
         case "-":
-            return subtraction(firstNum, lastNum);
+            total += secondNum;
+            break;
         case "*":
-            return multiplication(firstNum, lastNum);
+            total += secondNum;
+            break;
         case "/":
-            return division(firstNum, lastNum);
+            total += secondNum;
+            break;
     }
 }
-
-function addition(firstNum, lastNum) { return firstNum + lastNum };
-function subtraction(firstNum, lastNum) { return firstNum - lastNum };
-function multiplication(firstNum, lastNum) { return firstNum * lastNum };
-function division(firstNum, lastNum) { return firstNum / lastNum };
